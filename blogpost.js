44139ee5a2ff365e353a114360fbf8506e3138d2
Doug Huang <doug.huang@hotmail.com>
2017-02-11 10:20:17 -0800

    use node-uuid module to provide unique post_id

--- blogpost.js
+++ blogpost.js
@@ -1,12 +1,12 @@
 //Load modules
-var sqlite3         =       require('sqlite3').verbose();
-var db              =       new sqlite3.Database('./blog.db');
-
+var sqlite3  = require('sqlite3').verbose();
+var db       = new sqlite3.Database('./blog.db');
+var uuid 	 = require('node-uuid');
 
 //Perform INSERT operation.
-db.run("INSERT into posts(title,body) VALUES ('title 1', 'title 1 body')");
-db.run("INSERT into posts(title,body) VALUES ('title 2', 'title 2 body')");
-db.run("INSERT into posts(title,body) VALUES ('title 3', 'title 3 body')");
+db.run("INSERT into posts(title,body, post_id) VALUES ('title 1', 'title 1 body', '" + uuid.v1() + "')");
+db.run("INSERT into posts(title,body, post_id) VALUES ('title 2', 'title 2 body', '" + uuid.v1() + "')");
+db.run("INSERT into posts(title,body, post_id) VALUES ('title 3', 'title 3 body', '" + uuid.v1() + "')");
 
 //Perform SELECT Operation
 db.all("SELECT * from posts",function(err,rows){
